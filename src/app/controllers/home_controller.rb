8112dff6d47465f377f289e62414f49e6e9a8dac
Sébastien Putier <sputier@gmail.com>
2020-08-19 11:12:56 +0200

    Added Dashboard 1 to 3

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
-    def index
-        render
-        # render :json => [1, 2, 3]
+    def dashboard1()
+        render "dashboard1"
     end
+    def dashboard2()
+        render "dashboard2"
+    end
+    def dashboard3()
+        render "dashboard3"
+    end
+
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :home
+  # root 'home#dashboard1'
+  root controller: :home, action: :dashboard1
+
+  resources :home, only: [] do
+    collection do
+      get 'dashboard1', to: 'home#dashboard1'
+      get 'dashboard2', to: 'home#dashboard2'
+      get 'dashboard3', to: 'home#dashboard3'
+    end
+  end
 end
